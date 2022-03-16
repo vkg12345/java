e560c2828447a8ba941a3ac4a46523ee97e9be8a
vkg12345 <101549409+vkg12345@users.noreply.github.com>
2022-03-16 13:57:36 +0530

    Update JavaapiApplication.java

--- src/main/java/com/example/javaapi/JavaapiApplication.java
+++ src/main/java/com/example/javaapi/JavaapiApplication.java
@@ -3,11 +3,18 @@ package com.example.javaapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController  
 @SpringBootApplication
 public class JavaapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaapiApplication.class, args);
 	}
+	
+	@GetMapping("/api")  
+	private List<Books> getAllBooks()   
+	{  
+	return "deployed sucessfully first image";  
+	}  
 
 }
